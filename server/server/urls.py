9b279fcf3f7ebf459c2b5a8a74295d351306c427
Giancarlo <g.galvarini@alumno.um.edu.ar>
2021-07-11 13:06:19 -0300

    update

---
+++ server/server/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
+    path('api/', include('authentication.urls')),
+    path('api/', include(router.urls)),
+]
